bccdf7eb6231f1620c0d3c9a75ac1ff2ca0a5ec5
TPride <tranielmp8@gmail.com>
2023-12-13 18:56:47 -0600

    first comiit

---
+++ app.js
@@ -0,0 +1,20 @@
+const shareButtons = document.querySelectorAll('.tile-share-button')
+console.log(shareButtons)
+
+async function copyText(e) {
+    e.preventDefault();
+    const link = this.getAttribute('link')
+    console.log(link)
+
+    try {
+        await navigator.clipboard.writeText(link)
+        alert("Copied the text: " + link)
+        
+    } catch (err) {
+        console.error(err)
+    }
+
+    
+}
+
+shareButtons.forEach(shareButton => shareButton.addEventListener('click', copyText))
